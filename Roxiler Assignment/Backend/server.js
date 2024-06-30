6e75f4ec565725dcf7afb0521d9a9f99e3dc36e5
ayush064 <109851676+ayush064@users.noreply.github.com>
2024-06-30 23:54:57 +0530

    Delete Roxiler Assignment/Backend/server.js

--- Roxiler Assignment/Backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const productRoutes = require('./routes/productRoutes');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect('mongodb://localhost:27017/transactions', {
